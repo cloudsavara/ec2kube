84c2304b0debddcd2d3d66f3fea94d1b1594d151
cloudsavara <cloudsa.vara@gmail.com>
2022-11-09 06:35:11 +0100

    Update main.tf

--- instances/main.tf
+++ instances/main.tf
@@ -6,8 +6,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "<bucket_name>"
-    key    = "<state_name>"
+    bucket = "tfstateec2kube"
+    key    = "instances/terraform.tfstate"
     region = "us-east-1"
   }
 }
